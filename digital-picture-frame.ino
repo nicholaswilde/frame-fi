bf61b714c2d43f522bfeae301fae94f7027bf541
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-07-15 14:43:47 -0700

    Update digital-picture-frame.ino

--- digital-picture-frame.ino
+++ digital-picture-frame.ino
@@ -1,15 +1,12 @@
 #include "pin_config.h"
 #include "secrets.h"
 
-// The BOOT button is on GPIO0
-#define BOOT_BUTTON_PIN 0
-
 void setup() {
   Serial.begin(115200);
   delay(1000); // Wait for serial to initialize
 
   // Configure the BOOT button pin
-  pinMode(BOOT_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BTN_PIN, INPUT_PULLUP);
 
   Serial.println("LILYGO T-Dongle S3 - SFTP Server & USB Drive (SD_MMC Version)");
 }
