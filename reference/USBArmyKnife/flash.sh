09f3a31b91e7f576eb2e44979c706ea0e6d30707
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-07-15 07:34:50 -0700

    Update flash.sh

--- reference/USBArmyKnife/flash.sh
+++ reference/USBArmyKnife/flash.sh
@@ -4,8 +4,12 @@ esptool \
   --chip esp32s3 \
   --baud 921600 \
   --before default-reset \
-  --after hard-reset write-flash -z \
+  --after hard-reset write-flash \
+  -z \
   --flash-mode dio \
   --flash-freq 80m \
   --flash-size 16MB \
-  0x0 firmware.bin
+  0x0000  bootloader.bin \
+  0x8000  partitions.bin \
+  0xE000  boot_app0.bin \
+  0x10000 firmware.bin
