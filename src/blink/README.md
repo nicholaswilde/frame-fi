929b08e49e191034aa6f4efb97bfe7297f68adf1
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-07-16 12:48:22 -0700

    update
    Signed-off-by: nιcнolaѕ wιlde <ncwilde43@gmail.com>

---
+++ src/blink/README.md
@@ -0,0 +1,8 @@
+# blink
+
+Blink the LED using the FastLED library.
+
+## :link: References
+
+- <https://registry.platformio.org/libraries/fastled/FastLED/examples/Blink/Blink.ino>
+- <https://github.com/Xinyuan-LilyGO/T-Dongle-S3/blob/506071bac8bc523562d319c1f8f990e6b1c36694/example/Factory/Factory.ino>
--- src/blink/main.cpp
+++ src/blink/main.cpp
@@ -1,17 +1,27 @@
 #include <Arduino.h>
+#include <FastLED.h>
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
 
 void setup() {
+  Serial.begin(115200);
   // Initialize the LED pin as an output
-  pinMode(LED_DI_PIN, OUTPUT);
+  FastLED.addLeds<APA102, LED_DI_PIN, LED_CI_PIN, BGR>(leds, NUM_LEDS);
 }
 
 void loop() {
   // Turn the LED on
-  digitalWrite(LED_DI_PIN, HIGH);
-  // Wait for a second
+  leds[0] = CRGB::Green;
+  FastLED.show();
+  Serial.println("Green");
+// Wait for a second
   delay(1000);
+  
   // Turn the LED off
-  digitalWrite(LED_DI_PIN, LOW);
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  Serial.println("Off");
   // Wait for a second
-  delay(1000);
+  delay(1000);  
 } 
