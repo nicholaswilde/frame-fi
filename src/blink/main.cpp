051db8d5f8a7e4f3951f7a5fc2cfb288e6329192
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-07-16 10:40:32 -0700

    Update main.cpp

--- src/blink/main.cpp
+++ src/blink/main.cpp
@@ -1,20 +1,17 @@
 #include <Arduino.h>
 
-// Define the LED pin from the platformio.ini
-#define LED_PIN 40
-
 void setup() {
   // Initialize the LED pin as an output
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_DI_PIN, OUTPUT);
 }
 
 void loop() {
   // Turn the LED on
-  digitalWrite(LED_PIN, HIGH);
+  digitalWrite(LED_DI_PIN, HIGH);
   // Wait for a second
   delay(1000);
   // Turn the LED off
-  digitalWrite(LED_PIN, LOW);
+  digitalWrite(LED_DI_PIN, LOW);
   // Wait for a second
   delay(1000);
 } 
