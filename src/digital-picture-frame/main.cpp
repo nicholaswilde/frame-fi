a390616b06b7286c9635594f6498a7df31742797
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-07-16 11:46:18 -0700

    Update main.cpp

--- src/digital-picture-frame/main.cpp
+++ src/digital-picture-frame/main.cpp
@@ -1,16 +1,11 @@
 #include "secrets.h"
+#include "Arduino.h"
 
 void setup() {
   Serial.begin(115200);
-  delay(1000); // Wait for serial to initialize
-
-  // Configure the BOOT button pin
-  pinMode(BTN_PIN, INPUT_PULLUP);
-
-  Serial.println("LILYGO T-Dongle S3 - SFTP Server & USB Drive (SD_MMC Version)");
 }
 
 void loop() {
-  Serial.println("IP address");
+  Serial.println("Hello world!");
   delay(1000);
-} 
+}
