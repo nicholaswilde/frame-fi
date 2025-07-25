b093b5f2fc9251440396ca79e8fa7b5e66c1bf2e
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-07-25 14:54:48 -0700

    Update main.cpp

--- src/digital-picture-frame/main.cpp
+++ src/digital-picture-frame/main.cpp
@@ -1,4 +1,16 @@
-// // Include necessary libraries
+/******************************************
+ * digit-picture-frame
+ * ----------------
+ * Use a button to switch between mock FTP
+ * and MSC modes.
+ *
+ * @author Nicholas Wilde, 0xb299a622
+ * @date 25 Jul 2025
+ * @version 0.1.0
+ *
+ ******************************************/ 
+
+// Include necessary libraries
 #include "Arduino.h"
 #include <WiFi.h>
 #include <WebServer.h>
