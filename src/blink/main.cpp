841e3e80ad50483ebc03b04aaaa0926a69855707
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-07-25 14:45:27 -0700

    Update main.cpp

--- src/blink/main.cpp
+++ src/blink/main.cpp
@@ -1,13 +1,13 @@
-/*************************************
+/******************************************
  * blink
  * ----------------
- * Blink an LED
+ * Blink the LED using the FastLED library.
  *
  * @author Nicholas Wilde, 0xb299a622
  * @date 25 Jul 2025
  * @version 0.1.0
  *
- *************************************/
+ ******************************************/
 
 #include <Arduino.h>
 #include <FastLED.h>
