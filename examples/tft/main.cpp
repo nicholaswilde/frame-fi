32bcf8b7b78a1e81781b422215f1fa29da3ba296
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-07-30 12:17:05 -0700

    update
    Signed-off-by: nιcнolaѕ wιlde <ncwilde43@gmail.com>

--- examples/tft/main.cpp
+++ examples/tft/main.cpp
@@ -6,20 +6,31 @@
 
 TFT_eSPI tft = TFT_eSPI();
 
+#define PRINT_STR(str, x, y)                                                                                                                         \
+  do {                                                                                                                                               \
+    Serial.println(str);                                                                                                                             \
+    tft.drawString(str, x, y);                                                                                                                       \
+    y += 8;                                                                                                                                          \
+  } while (0);
+
 void setup(){
   int32_t x, y;
   Serial.begin(115200);
   Serial.println("Hello T-Dongle-S3");
+  pinMode(38, OUTPUT);
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
+  digitalWrite(38, 0);
   tft.setTextFont(1);
   tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  PRINT_STR("Hello World!", x, y)
+  PRINT_STR("Hello World2!", x, y)
 }
 
 void loop(){ 
-  tft.setCursor(0, 0, 2);
-  tft.println("Hello World!");
+  // tft.setCursor(0, 0, 2);
+  // tft.println("Hello World!");
   Serial.println("test");
   delay(1000);
 }
