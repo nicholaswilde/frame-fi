4c2811d58b3b136ad77d02b60603e29c8cfa9740
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-07-30 16:52:06 -0700

    Update main.cpp

--- examples/tft/main.cpp
+++ examples/tft/main.cpp
@@ -7,6 +7,9 @@
 
 TFT_eSPI tft = TFT_eSPI();
 
+// Prototypes
+void drawApModeScreen(const char* ap_ssid, const char* ap_ip);
+
 #define PRINT_STR(str, x, y)                                                                                                                         \
   do {                                                                                                                                               \
     Serial.println(str);                                                                                                                             \
@@ -33,3 +36,26 @@ void loop(){
   Serial.println("test");
   delay(1000);
 }
+
+void drawApModeScreen(const char* ap_ssid, const char* ap_ip) {
+  tft.fillScreen(CATPPUCCIN_BASE);
+  drawHeader("FrameFi Setup");
+
+  // Display instructions centered on the screen
+  int y_pos = 30;
+  tft.setTextColor(CATPPUCCIN_TEXT);
+  tft.drawCentreString("Connect to WiFi:", 80, y_pos, 1);
+  y_pos += 12;
+  
+  tft.setTextColor(CATPPUCCIN_YELLOW);
+  tft.drawCentreString(ap_ssid, 80, y_pos, 2); // Larger font for SSID
+  y_pos += 18;
+
+  tft.setTextColor(CATPPUCCIN_TEXT);
+  tft.drawCentreString("Go to http://", 80, y_pos, 1);
+  y_pos += 12;
+  
+  tft.setTextColor(CATPPUCCIN_GREEN);
+  tft.drawCentreString(ap_ip, 80, y_pos, 2);
+}
+
