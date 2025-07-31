3e7a929a4730b6c9e522ab5abce0c0044f08011c
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-07-31 14:30:44 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -37,9 +37,6 @@
 #include "TFT_eSPI.h" // https://github.com/Bodmer/TFT_eSPI
 
 // --- LED Configuration ---
-// #define NUM_LEDS 1
-// #define LED_DI_PIN 40
-// #define LED_CI_PIN 39
 #define BRIGHTNESS 13 // 5% of 255
 
 // --- Button Configuration ---
